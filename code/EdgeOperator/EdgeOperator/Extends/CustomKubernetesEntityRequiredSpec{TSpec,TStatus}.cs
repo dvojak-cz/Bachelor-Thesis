f2865b9d373e9e0bbdf68f431a6f2bd6eb590b2f
Jan Trojak <trojakjan24@gmail.com>
2023-03-08 00:24:47 +0100

    Git add CRDEntityRequiredSpec fix
    // This is just to make spec in the CRD required (in json schema) see PR [#520](https://github.com/buehler/dotnet-operator-sdk/pull/520)

---
+++ code/EdgeOperator/EdgeOperator/Extends/CustomKubernetesEntityRequiredSpec{TSpec,TStatus}.cs
@@ -0,0 +1,10 @@
+namespace cz.dvojak.k8s.EdgeOperator.Extends;
+
+// This is just to make spec in the CRD required (in json schema) see PR [#520](https://github.com/buehler/dotnet-operator-sdk/pull/520)
+public abstract class CustomKubernetesEntityRequiredSpec<TSpec,TStatus> 
+    : CustomKubernetesEntityRequiredSpec<TSpec>
+    where TSpec : new()
+    where TStatus : new()
+{
+    public TStatus Status { get; set; } = new TStatus();
+}
