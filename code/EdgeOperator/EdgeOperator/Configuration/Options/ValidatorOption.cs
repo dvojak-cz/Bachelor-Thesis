a7bbd10940229d6b1e52ddd5f243601706798789
Jan Troják <trojakjan24@gmail.com>
2023-04-16 01:58:22 +0200

    Add submodule support to GH actions (#6)

--- code/EdgeOperator/EdgeOperator/Configuration/Options/ValidatorOption.cs
+++
@@ -1,9 +0,0 @@
-namespace cz.dvojak.k8s.EdgeOperator.Configuration.Options;
-
-public class ValidatorOption
-{
-    public const string VALIDATOR = "Validator";
-
-    public bool DeviceStrict { get; set; }
-    public bool ConnectionStrict { get; set; }
-}
--- code/EdgeOperator/EdgeOperator/Program.cs
+++
@@ -1,9 +0,0 @@
-using cz.dvojak.k8s.EdgeOperator.Configuration;
-using KubeOps.Operator;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.ConfigureEdgeOperatorProject();
-var app = builder.Build();
-app.ConfigureEdgeOperatorApp();
-app.PreRunLogging();
-await app.RunOperatorAsync(args);
